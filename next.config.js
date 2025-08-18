31c28d373482a1939472208e3bd1b639bd4cbb8b
Paritybitz <ali.mohsin05@gmail.com>
2025-08-18 00:31:18 -0400

    chore: update Next.js configuration for production deployment
    - Added conditional basePath for production environment in next.config.js.
    - Ensured proper formatting and structure in the configuration file.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/phg-app' : '',
     output: 'export',
     distDir: 'dist',
     images: {
         unoptimized: true,
     },
-  };
+};
    
-  module.exports = nextConfig;
+module.exports = nextConfig;
